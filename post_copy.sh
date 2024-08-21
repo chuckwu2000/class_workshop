9f7c894e9b5cd0089c5761f592ec5958dac51f8f
chuckwu2000 <chuckwu92@gmail.com>
2024-08-21 15:54:20 +0800

    [For copy data into ml-embedded-evaluation-kit]
    * Add two shell script to copy data inot ml-embedded-evaluation-kit

---
+++ post_copy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r ./resources/img_person_detect ./ml-embedded-evaluation-kit/resources
+cp -r ./source/use_case/img_person_detect ./ml-embedded-evaluation-kit/source/use_case
+cp -r ./vela/img_person_detect ./ml-embedded-evaluation-kit/resources_downloaded/
+cp -r ./resources/img_yolofastest_relu6_256_himax ./ml-embedded-evaluation-kit/resources
+cp -r ./source/use_case/img_yolofastest_relu6_256_himax ./ml-embedded-evaluation-kit/source/use_case/
+cp -r ./vela/img_yolofastest_relu6_256_himax ./ml-embedded-evaluation-kit/resources_downloaded/
+cp -r ./resources/img_yolofastest_xl_relu6_256_himax ./ml-embedded-evaluation-kit/resources
+cp -r ./source/use_case/img_yolofastest_xl_relu6_256_himax ./ml-embedded-evaluation-kit/source/use_case
+cp -r ./vela/img_yolofastest_xl_relu6_256_himax ./ml-embedded-evaluation-kit/resources_downloaded/
